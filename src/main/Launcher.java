fa84fcb10f5b9ae59c59de068fab9ff82472e4c8
deadlycrawler <deadlycrawler2@gmail.com>
2017-08-28 12:39:39 +0000

    Initial commit

---
+++ src/main/Launcher.java
@@ -0,0 +1,12 @@
+package main;
+
+import Display.Display;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        Display display = new Display("title",400,400);
+    }
+
+}
